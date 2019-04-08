029267e3b5abb70e51205629a417aa9b501077f9
Oana Tanasoiu <oana.tanasoiu@oanas-mbp.wifi.tsr.corp.puppet.net>
2019-04-08 13:32:12 +0300

    (PA-2592) Create 'log' dir in case it doesn't exists

--- lib/release_manager.rb
+++ lib/release_manager.rb
@@ -10,8 +10,10 @@ module ReleaseManager
   AGENT_URL      = 'git@github.com:puppetlabs/puppet-agent.git'
   COMPONENTS_DIR = RELEASE_DIR.join('pkg')
   AGENT_DIR      = RELEASE_DIR.join('puppet-agent')
+  LOG_DIR        = ROOT_DIR.join('log')
 
   def self.logger
+    Helpers::File.create_dir(LOG_DIR) unless Helpers::File.dir_exists?(LOG_DIR)
     @logger ||= Logger.new(File.open(ROOT_DIR.join('log', 'release_manager.log'), 'a'))
   end
 end
